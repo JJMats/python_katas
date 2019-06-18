7c01ac8931b7350bb2e12b9783426d5abef926f9
J2Tuner <Jeremy@KroyerRacingEngines.com>
2019-06-18 15:24:49 -0700

    feat: Add binary search practice problem

--- data_structures_and_algorithms/004_binary_search/search.py
+++ data_structures_and_algorithms/004_binary_search/search.py
@@ -12,10 +12,26 @@ doesn't exist in the list."""
 
 def binary_search(input_array, value):
     """Your code goes here."""
+    input_array.sort()
+    
+    min_idx = 0
+    max_idx = len(input_array) - 1
+
+    while(min_idx <= max_idx):
+        mid_idx = int((min_idx + max_idx) / 2)
+        if input_array[mid_idx] == value:
+            return mid_idx
+        elif input_array[mid_idx] < value:
+            min_idx = mid_idx + 1
+        else:
+            max_idx = mid_idx - 1
+
     return -1
 
-test_list = [1,3,9,11,15,19,29]
+test_list = [1,3,9,11,15,19,25,29]
 test_val1 = 25
 test_val2 = 15
+test_val3 = 2
 print(binary_search(test_list, test_val1))
 print(binary_search(test_list, test_val2))
+print(binary_search(test_list, test_val3))
